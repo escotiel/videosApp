66b860428f52630a0cce37fc059cb9dda6d11285
robsonesr@gmail.com <robsonesr@gmail.com>
2021-05-31 13:14:52 -0300

    Adicionado cor vermelha (Atividade 3ª Semana)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,32 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+
+  titulo = 'Videos Appp';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Anônimo (2021)',
+      lancamento: '12/05/2021 (BR)',
+      duracao: '1h32min',
+      classificacao: 85,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/woPYFGKwVULILbO7q44HmsNY0kO.jpg',
+      generos: [
+        'Ação', 'Thriller', 'Crime', 'Comédia'
+      ]
+    },
+    {
+      nome: 'Infiltrado (2021)',
+      lancamento: '27/05/2021 (BR)',
+      duracao: '1h59min',
+      classificacao: 79,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/dAtAp4IeT6EYGhewfnNNhalobme.jpg',
+      generos: [
+        'Ação', 'Crime'
+      ]
+    },
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
